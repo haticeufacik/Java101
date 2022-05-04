1fdf9ee43a1c6ae95dded8af358d6df3919dc669
Hatice UFACIK <haticeufacik@gmail.com>
2022-05-04 21:10:01 +0300

    First Commit

---
+++ src/NotOrtalamasi.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+        double ort;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu giriniz.");
+        mat = inp.nextInt();
+
+        System.out.println("Fizik notunuzu giriniz.");
+        fizik = inp.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz.");
+        kimya = inp.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz.");
+        turkce = inp.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz.");
+        tarih = inp.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz.");
+        muzik = inp.nextInt();
+
+        ort = ((mat + fizik + kimya + turkce + tarih + muzik) / 6);
+
+        System.out.println("ortalamaniz : " + ort);
+        String sonuc = (ort >= 50) ? "Sinifi gecti" : "Sinifta kaldi";
+        System.out.println("Sinifi gecme durumu : " + sonuc);
+    }
+}
